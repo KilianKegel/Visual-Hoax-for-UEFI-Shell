d1b5f4a73959082051d139616bedd522d69020ae
KG <kilian_kegel@hotmail.com>
2020-02-04 10:04:09 +0100

    ### 20200204/R154 * Update to torito-C 20200204/R154 * adjust solution configurations * update copyright

--- clean.bat
+++ clean.bat
@@ -1,19 +1,8 @@
 @echo off
 echo clean up...
 
-if exist %~p0x64\Efi64\*.pdb del /s %~p0x64\Efi64\*.pdb
-if exist %~p0x64\Efi64\*.map del /s %~p0x64\Efi64\*.map
-if exist %~p0x64\Efi64\*.ilk del /s %~p0x64\Efi64\*.ilk
+if exist x64 del /s x64\*.pdb
+if exist x64 del /s x64\*.map
+for /D %%d in (*) do if exist %%d\x64 rd /s /q %%d\x64
+for /D /R %%d in (doxygen.tmp) do if exist %%d rd /s /q %%d
 
-if exist %~p0x64\Win64\*.pdb del /s %~p0x64\Win64\*.pdb
-if exist %~p0x64\Win64\*.map del /s %~p0x64\Win64\*.map
-if exist %~p0x64\Win64\*.ilk del /s %~p0x64\Win64\*.ilk
-
-if exist %~p0x64\Win64.msft\*.pdb del /s %~p0x64\Win64.msft\*.pdb
-if exist %~p0x64\Win64.msft\*.map del /s %~p0x64\Win64.msft\*.map
-if exist %~p0x64\Win64.msft\*.ilk del /s %~p0x64\Win64.msft\*.ilk
-
-if exist %~p0Debug rd /s /q %~p0Debug
-
-for /F  %%d in ('dir /b /AD') do if exist %%d\x64 rd /s /q %%d\x64
-for /F  %%d in ('dir /b /AD') do if exist %%d\doxygen.tmp rd /s /q %%d\doxygen.tmp
